ce12a675574305d9943350a5055562ded81f551a
abdullahshaaban <abdullah.shaaban91@gmail.com>
2017-09-10 20:57:27 -0700

    added mutler code

--- server/routes/image.js
+++ server/routes/image.js
@@ -1,7 +1,15 @@
 const express = require('express');
+//require multer for the file uploads
+var multer = require('multer');
+// set the directory for the uploads to the uploaded to
+var DIR = '../uploads/';
+
+
 const router = express.Router();
 
 const Image = require('../models/image');
+
+var upload = multer({dest: DIR}).single('photo');
 
 router.get('/image', (req, res, next)=>{
 	Image.find(function(err, resultimage){
@@ -27,5 +35,17 @@ router.post('/image',(req, res, next)=>{
 	});
 });
 
+router.post('/fileupload', function (req, res, next) {
+    var path = '';
+    upload(req, res, function (err) {
+       if (err) {
+         // An error occurred when uploading
+         console.log(err);
+         return res.status(422).send("an Error occured")
+       }  
+      // No error occured.
+       path = req.file.path;
+       return res.send("Upload Completed for "+path); 
+ });  
 
 module.exports = router;
